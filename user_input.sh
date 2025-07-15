50083d512d3c8ccdb369c88f92511065347d58af
Eswar Gumparlapati <gumparlapatieswar@gmail.com>
2025-07-15 11:24:42 +0530

    Add script to take and display user input

---
+++ user_input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# File: user_input.sh
+#Description: Take user input and prints a welcome message.
+# Author: Eswar Gumparlapati
+# Created on : 2025-07-15
+
+read -p "Enter your name: " name
+echo "Welcome, $name !"
