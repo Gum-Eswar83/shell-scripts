35bd9204f8b9473ba133472854c162e2af8f0e39
Eswar Gumparlapati <gumparlapatieswar@gmail.com>
2025-07-15 11:05:45 +0530

    Add hello world shell script

---
+++ hello_world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# File: helloworld.sh
+#Description: This script prints "Hello, World!" to the terminal.
+# Author: Eswar Gumparlapati
+# Created on : 2025-07-15
+
+echo "Hello, World!"
